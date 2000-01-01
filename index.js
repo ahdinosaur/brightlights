653872ecef5d73b0e951e8d8640ef6514afb58e9
root <root@beaglebone.(none)>
2000-01-01 00:03:26 +0000

    got ws2801's working with node-spi

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function(){
+/*(function(){
   var osc = require('osc-min'),
       udp = require('dgram');
 
@@ -13,4 +13,16 @@
     }
   });
   sock.bind(inport);
-}).call(this);
+}).call(this);*/
+
+var numPixels = 32;
+
+var _ = require('lodash'),
+    ws2801 = require('./ws2801');
+
+var strip = new ws2801.Strip('/dev/spidev2.0', numPixels);
+
+//pixel.all(30, 30, 30);
+//pixel.sync();
+
+strip.throb(_.range(numPixels), [0,0,0], [255,100,100], 1, {})
